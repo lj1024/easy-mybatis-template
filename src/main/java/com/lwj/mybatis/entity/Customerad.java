2f32f987efc7ddb41700096ef6527dda4459e4ab
lj1024 <360476974@qq.com>
2018-10-11 16:13:20 +0800

    easy-mybatis-template init

---
+++ src/main/java/com/lwj/mybatis/entity/Customerad.java
@@ -0,0 +1,126 @@
+package com.lwj.mybatis.entity;
+
+import com.lwj.easymybatis.entity.Entity;
+import java.util.Date;
+
+public class Customerad extends Entity {
+    private String agent;
+
+    private String contactor;
+
+    private String telephone;
+
+    private String month;
+
+    private String customerName;
+
+    private String product;
+
+    private String location;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private String payment;
+
+    private String cost;
+
+    private String remark;
+
+    public String getAgent() {
+        return agent;
+    }
+
+    public void setAgent(String agent) {
+        this.agent = agent == null ? null : agent.trim();
+    }
+
+    public String getContactor() {
+        return contactor;
+    }
+
+    public void setContactor(String contactor) {
+        this.contactor = contactor == null ? null : contactor.trim();
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone == null ? null : telephone.trim();
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month == null ? null : month.trim();
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName == null ? null : customerName.trim();
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product == null ? null : product.trim();
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location == null ? null : location.trim();
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment == null ? null : payment.trim();
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost == null ? null : cost.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+}
---
+++ src/main/java/com/lwj/mybatis/mapper/CustomeradMapper.java
@@ -0,0 +1,7 @@
+package com.lwj.mybatis.mapper;
+
+import com.lwj.easymybatis.common.CommonMaper;
+import com.lwj.mybatis.entity.Customerad;
+
+public interface CustomeradMapper extends CommonMaper<Customerad> {
+}
